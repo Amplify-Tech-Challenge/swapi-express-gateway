ea822f91e52c870f9370e684e44ad946563d5af1
Scott Brabson <scottbrabson@gmail.com>
2021-04-10 00:56:42 -0600

    no POST - remove val, fix response w/json()

--- routes/characters.js
+++ routes/characters.js
@@ -13,35 +13,37 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const character = await fetch(`${endpoint}/${id}`);
+  const request = await fetch(`${endpoint}/${id}`);
+  const fetchedCharacter = await request.json();
 
-  if (!character) return res.status(404).send("No character with ID: " + id);
+  if (!fetchedCharacter)
+    return res.status(404).send("No character with ID: " + id);
 
-  const { error } = validateRequest(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  // const { error } = validateRequest(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
 
-  res.send(character);
+  res.send(fetchedCharacter);
   // do I need to do this? I may not for SSG
   // const character = characters.find
 });
 
-const validateRequest = character => {
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    height: Joi.string().required(),
-    mass: Joi.string().required(),
-    hair_color: Joi.string().required(),
-    skin_color: Joi.string().required(),
-    eye_color: Joi.string().required(),
-    birth_year: Joi.string().required(),
-    gender: Joi.string().required(),
-    homeworld: Joi.string().required(),
-    films: Joi.array().required(),
-    species: Joi.array().required(),
-    vehicles: Joi.array().required(),
-    starships: Joi.array().required(),
-  });
-  return schema.validate(character);
-};
+// const validateRequest = character => {
+//   const schema = Joi.object({
+//     name: Joi.string().required(),
+//     height: Joi.string().required(),
+//     mass: Joi.string().required(),
+//     hair_color: Joi.string().required(),
+//     skin_color: Joi.string().required(),
+//     eye_color: Joi.string().required(),
+//     birth_year: Joi.string().required(),
+//     gender: Joi.string().required(),
+//     homeworld: Joi.string().required(),
+//     films: Joi.array().required(),
+//     species: Joi.array().required(),
+//     vehicles: Joi.array().required(),
+//     starships: Joi.array().required(),
+//   });
+//   return schema.validate(character);
+// };
 
 module.exports = router;
