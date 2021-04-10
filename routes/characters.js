97d7644c53db4c8fae182103988f5923366b87c8
Scott Brabson <scottbrabson@gmail.com>
2021-04-10 00:58:01 -0600

    remove comments

--- routes/characters.js
+++ routes/characters.js
@@ -16,34 +16,9 @@ router.get("/:id", async (req, res) => {
   const request = await fetch(`${endpoint}/${id}`);
   const fetchedCharacter = await request.json();
 
-  if (!fetchedCharacter)
-    return res.status(404).send("No character with ID: " + id);
-
-  // const { error } = validateRequest(req.body);
-  // if (error) return res.status(400).send(error.details[0].message);
+  if (!fetchedCharacter) return res.status(404).send("No character with ID: " + id);
 
   res.send(fetchedCharacter);
-  // do I need to do this? I may not for SSG
-  // const character = characters.find
 });
-
-// const validateRequest = character => {
-//   const schema = Joi.object({
-//     name: Joi.string().required(),
-//     height: Joi.string().required(),
-//     mass: Joi.string().required(),
-//     hair_color: Joi.string().required(),
-//     skin_color: Joi.string().required(),
-//     eye_color: Joi.string().required(),
-//     birth_year: Joi.string().required(),
-//     gender: Joi.string().required(),
-//     homeworld: Joi.string().required(),
-//     films: Joi.array().required(),
-//     species: Joi.array().required(),
-//     vehicles: Joi.array().required(),
-//     starships: Joi.array().required(),
-//   });
-//   return schema.validate(character);
-// };
 
 module.exports = router;
