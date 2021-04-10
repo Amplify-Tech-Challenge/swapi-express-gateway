5ea8fc669857eb7a36884346b32438da888124d9
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 23:48:42 -0600

    add logic to get characters

--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const Joi = require("joi")
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const allCharacters = require("../utils/getCharacters");
+const fetch = require("node-fetch");
 
-router.get("/", (req, res) => {
-  res.send({message: "hello"})
-})
+
+router.get("/", async (req, res) => {
+  const data = await allCharacters("test");
+  res.send(data);
+});
 
 module.exports = router;
