856b09ac77ca4b0bd1c66241f0d3c122c340ee91
Scott Brabson <scottbrabson@gmail.com>
2021-04-09 23:12:31 -0600

    add routes + json middleware

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000;
+const characters = require('./routes/characters')
+
+app.use(express.json())
+app.use('/api/characters', characters)
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
