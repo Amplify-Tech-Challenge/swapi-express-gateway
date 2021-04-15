98375a6174bc1ee7105bf2e7d16d14c7f9688a11
Scott Brabson <scottbrabson@gmail.com>
2021-04-15 16:56:35 -0600

    add CORS and err handling middleware

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const app = express()
-const characters = require('./routes/characters')
-const path = require('path')
+, app = express()
+, characters = require('./routes/characters')
+, cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'));
-// app.use(express.static((path.join(__dirname, 'public'))));
 app.use('/api/characters', characters)
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send("I've got a bad feeling about this...there was an error, contact scottbrabson@gmail.com")
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
